6732633710a6b187fa3eba10205b6db412221c4d
junsong-cang <cangjunsong@outlook.com>
2023-12-08 01:35:25 +0800

    I donno what i changed about version

--- src/py21cmfast/_version.py
+++ src/py21cmfast/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '3.2.1.dev47+ge7d006e.d20231205'
-__version_tuple__ = version_tuple = (3, 2, 1, 'dev47', 'ge7d006e.d20231205')
+__version__ = version = '3.2.1.dev50+gd945399.d20231207'
+__version_tuple__ = version_tuple = (3, 2, 1, 'dev50', 'gd945399.d20231207')
